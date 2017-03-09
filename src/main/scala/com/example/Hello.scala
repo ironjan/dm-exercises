591cb9538fe2a73facf4fa73a95113f61ffa46ec
Jan Lippert <lippertsjan@gmail.com>
2017-03-09 14:58:41 +0200

    Reading file;

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -2,6 +2,21 @@ package com.example
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    println("registration_year [course_year_and_month course_code \"course name\" credits final_grade ]*")
+    val bufferedSource = io.Source.fromFile("./data-2016.csv")
+    for (line <- bufferedSource.getLines) {
+      val cols = line.split(" ").map(_.trim).toList
+
+      val registration_year = cols.head.toInt
+      println(s"Registration: $registration_year")
+
+      val registration_set = cols.tail.sliding(5).toList
+
+      for(set <- registration_set){
+        println(s" $set")
+      }
+
+    }
+    bufferedSource.close
   }
 }
