cfaf3a78a1a80c384a8ad71d58e188d8147f3343
Jan Lippert <lippertsjan@gmail.com>
2017-03-09 21:56:51 +0200

    Added more docs and counting supports

--- 2/src/main/scala/DataMiningUtils.scala
+++ 2/src/main/scala/DataMiningUtils.scala
@@ -3,19 +3,39 @@
   * @tparam T
   */
 class DataMiningUtils[T](D: List[Set[T]]) {
-  
+
+  /**
+    * Determines the confidence of the given rule
+    * @param rule tuple of the form (A,B)
+    * @return the confidence of "A -> B"
+    */
   def confidence(rule: (Set[T], Set[T])) = {
     supportOfRuleInD(rule) / supportOfSetInD(rule._1)
   }
 
+  /**
+    * Determines the support of the given set
+    * @param set the set to count
+    * @return the support of above set
+    */
   def supportOfSetInD(set: Set[T]) = {
     D.count(transaction => set.subsetOf(transaction)).toDouble / N
   }
 
+  /**
+    * Determines the support of the given rule
+    * @param rule tuple of the form (A,B)
+    * @return the support of "A -> B"
+    */
   def supportOfRuleInD(rule: (Set[T], Set[T])) = {
     supportCountOfRuleInD(rule).toDouble / N
   }
 
+  /**
+    * Determines the support count of the given rule
+    * @param rule tuple of the form (A,B)
+    * @return the support count of "A -> B"
+    */
   def supportCountOfRuleInD(rule: (Set[T], Set[T])) = {
     val union = rule._1 union rule._2
     D.count(transaction => union.subsetOf(transaction))
@@ -32,6 +52,7 @@ class DataMiningUtils[T](D: List[Set[T]]) {
     * @return the item set of D
     */
   def IS = D.flatten.distinct.toSet
+
 
 }
 
