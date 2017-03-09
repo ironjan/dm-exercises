a62daf2716de31712066fce6b281c865a6bd8c8b
Jan Lippert <lippertsjan@gmail.com>
2017-03-09 15:26:25 +0200

    Did ex1

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,15 +1,31 @@
 package com.example
 
 /**
-  * 1. Write an application that reads in the dataset, and prints out all the different course_codes in the file.
   * 2. Create a program (or a method) that counts the number of students that have attempted the course "Introduction to programming"
   * 3. Create a program (or a method) that counts the number of students that have passed the course "Introduction to programming"
   * 4. Create a program (or a method) that counts the number of students that have passed the course "Advanced programming"
   * 5. Create a program (or a method) that counts the number of students that have passed the course "Data structures" with a grade 4
   */
 object Hello {
+
+
   def main(args: Array[String]): Unit = {
     val D = parseDataSet(readDataSet)
+    printDifferentCourseCodes(D)
+  }
+
+  /**
+    * 1. Write an application that [...] prints out all the different course_codes in the file.
+    */
+  private def printDifferentCourseCodes(D: List[Registration]): Unit ={
+    println("Course Codes:")
+
+    val printed = D.flatMap(r => r.courseRecords)
+      .map(_.code)
+      .distinct
+        .sorted
+      .mkString(", ")
+      println(s"$printed")
   }
 
   private def parseDataSet(D: List[(String, List[List[String]])]) = {
@@ -28,7 +44,8 @@ object Hello {
 
       val registration_year = cols.head
 
-      val registration_set = cols.tail.sliding(5).toList
+      val registration_set = cols.tail.sliding(5, 5).toList
+
 
       D = (registration_year, registration_set) :: D
     }
