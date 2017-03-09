ffcdc71c19dc74c14d389c410b0f64fb17dc2858
Jan Lippert <lippertsjan@gmail.com>
2017-03-09 15:44:55 +0200

    Did some more

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,17 +1,26 @@
 package com.example
 
-/**
-  * 2. Create a program (or a method) that counts the number of students that have attempted the course "Introduction to programming"
-  * 3. Create a program (or a method) that counts the number of students that have passed the course "Introduction to programming"
-  * 4. Create a program (or a method) that counts the number of students that have passed the course "Advanced programming"
-  * 5. Create a program (or a method) that counts the number of students that have passed the course "Data structures" with a grade 4
-  */
-object Hello {
 
+object Hello {
+  val IntroductionToProgramming = "Ohjelmoinnin_perusteet"
+  val AdvancedProgramming = "Aineopintojen_harjoitusty_Ohjelmointi"
+  val DataStructures = "Tietorakenteet_ja_algoritmit"
 
   def main(args: Array[String]): Unit = {
     val D = parseDataSet(readDataSet)
     printDifferentCourseCodes(D)
+
+    // 2. Create a program (or a method) that counts the number of students that have attempted the course "Introduction to programming"
+    countStudentsWith(D, IntroductionToProgramming, 0)
+
+    // 3. Create a program (or a method) that counts the number of students that have passed the course "Introduction to programming"
+    countStudentsWith(D, IntroductionToProgramming, 1)
+
+    // 4. Create a program (or a method) that counts the number of students that have passed the course "Advanced programming"
+    countStudentsWith(D, AdvancedProgramming, 1)
+
+    // 5. Create a program (or a method) that counts the number of students that have passed the course "Data structures" with a grade 4
+    countStudentsWith(D, DataStructures, 4)
   }
 
   /**
@@ -26,6 +35,21 @@ object Hello {
         .sorted
       .mkString(", ")
       println(s"$printed")
+  }
+
+  def countStudentsWith(D: List[Registration], s: String, minGrade: Int) = {
+    val number =
+      D.map{d =>
+        val filteredRecords =
+          d.courseRecords
+            .filter(_.name == s)
+            .filter(_.finalGrade > minGrade)
+
+        d.copy(courseRecords = filteredRecords)
+      }
+      .count(_.courseRecords.nonEmpty)
+
+    println(s"$number students had min grade $minGrade in `$s`.")
   }
 
   private def parseDataSet(D: List[(String, List[List[String]])]) = {
@@ -57,7 +81,7 @@ object Hello {
   def toCourseRecord(raw: Array[String]): CourseRecord =
     CourseRecord(raw(0),
       raw(1).toInt,
-      raw(2),
+      raw(2).tail.init,
       raw(3).toDouble,
       raw(4).toInt)
 }
