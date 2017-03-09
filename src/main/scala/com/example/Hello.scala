37d0ff1f725537670ba388c0a4ec16fd4c1d87a6
Jan Lippert <lippertsjan@gmail.com>
2017-03-09 15:03:40 +0200

    Separated reading and printing

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -2,21 +2,29 @@ package com.example
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("registration_year [course_year_and_month course_code \"course name\" credits final_grade ]*")
+    val D = readDataSet
+    
+    D.foreach{d =>
+      println(s"${d._1}")
+      d._2.map(x => "  " + x.mkString(", "))
+        .foreach(println)
+    }
+  }
+
+  def readDataSet = {
+    var D = List[(String, List[List[String]])]()
     val bufferedSource = io.Source.fromFile("./data-2016.csv")
     for (line <- bufferedSource.getLines) {
       val cols = line.split(" ").map(_.trim).toList
 
-      val registration_year = cols.head.toInt
-      println(s"Registration: $registration_year")
+      val registration_year = cols.head
 
       val registration_set = cols.tail.sliding(5).toList
 
-      for(set <- registration_set){
-        println(s" $set")
-      }
-
+      D = (registration_year, registration_set) :: D
     }
     bufferedSource.close
+
+    D
   }
 }
